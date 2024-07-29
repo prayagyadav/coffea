3ec2cf64d9f1591f051b6b1718999d2a8d0a1c39
prayagyadav <prayagyadav206@gmail.com>
2024-07-29 14:02:33 +0530

    automated commit from fedora

--- src/coffea/nanoevents/schemas/fcc.py
+++ src/coffea/nanoevents/schemas/fcc.py
@@ -7,13 +7,6 @@ from coffea.nanoevents.methods.fcc import RecoParticle
 from coffea.nanoevents.schemas.base import BaseSchema, nest_jagged_forms, zip_forms
 from coffea.nanoevents.util import concat
 
-# magic numbers currently used to define cross references
-# this will be updated later to make it more general
-RECO_PARTICLES = 0
-MC_PARTICLES = 1
-
-TRACKSTATE_XREF = 1
-
 _base_collection = re.compile(r".*[\#\/]+.*")
 _trailing_under = re.compile(r".*_[0-9]")
 _idxs = re.compile(r".*[\#]+.*")
@@ -23,32 +16,9 @@ class FCCSchema(BaseSchema):
     """FCC schema builder
 
     The FCC schema is built from all branches found in the supplied file,
-    based on the naming pattern of the branches. There are two steps of to the
-    generation of array collections:
+    based on the naming pattern of the branches.
 
-    - Objects with vector-like quantities (momentum, coordinate points) in the
-      TreeMaker ntuples are stored using ROOT PtEtaPhiEVectors and XYZPoint
-      classes with maximum TTree splitting. These variable branches are grouped
-      into a single collection with the original object name, with the
-      corresponding coordinate variables names mapped to the standard variable
-      names for coffea.nanoevents.methods.vector behaviors. For example:
-      - The "Jets" branch in a TreeMaker Ntuple branch stores 'PtEtaPhiEVector's
-        corresponding to the momentum of AK4 jets. The resulting collection after
-        this first step would contain the vector variables in the form of
-        Jets.pt, Jets.eta, Jets.phi, Jets.energy, and additional vector quantities
-        (px) can be accessed via the usual vector behavior methods.
-      - The "PrimaryVertices" branch in a TreeMaker Ntuple branch stores
-        'XYZPoint's corresponding to the coordinates of the primary vertices, The
-        resulting collection after this first step would contain the coordinate
-        variables in the form of PrimaryVertices.x, PrimaryVertices.y,
-        PrimaryVertices.z.
-
-    - Extended quantities of physics objects are stored in the format
-      <Object>_<variable>, such as "Jets_jecFactor". Such variables will be
-      merged into the collection <Object>, so the branch "Jets_jetFactor" will be
-      access to in the array format as "Jets.jecFactor". An exception to the??
-
-    All collections are then zipped into one `base.NanoEvents` record and
+    All collections are zipped into one `base.NanoEvents` record and
     returned.
     """
 
