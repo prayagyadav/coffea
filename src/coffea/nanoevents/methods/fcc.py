cfc779aa21f95fa451c88eebe0ac4abbbf0014f2
prayagyadav <prayagyadav206@gmail.com>
2024-09-04 16:37:38 +0530

    working parents and daughters in both the modes

--- src/coffea/nanoevents/methods/fcc.py
+++ src/coffea/nanoevents/methods/fcc.py
@@ -77,6 +77,29 @@ def map_index_to_array(array, index, axis=1):
     else:
         raise AttributeError('Only axis = 1 or axis = 2 supported at the moment.')
 
+@numba.njit
+def index_range_numba_wrap(begin_end, builder):
+    for ev in begin_end:
+        builder.begin_list()
+        for j in ev:
+            builder.begin_list()
+            for k in range(j[0],j[1]):
+                builder.integer(k)
+            builder.end_list()
+        builder.end_list()
+    return builder
+
+def index_range(begin, end):
+    begin_end = awkward.concatenate((begin[:,:,numpy.newaxis],end[:,:,numpy.newaxis]),axis=2)
+    if awkward.backend(begin) == "typetracer" or awkward.backend(end) == "typetracer":
+        # here we fake the output of numba wrapper function since
+        # operating on length-zero data returns the wrong layout!
+        # We need the axis 2, therefore, we should return the typetracer layout of [[[]]]
+        awkward.typetracer.length_zero_if_typetracer(begin) # force touching of the necessary data
+        awkward.typetracer.length_zero_if_typetracer(end) # force touching of the necessary data
+        return awkward.Array(awkward.Array([[[]]]).layout.to_typetracer(forget_length=True))
+    return index_range_numba_wrap(begin_end, awkward.ArrayBuilder()).snapshot()
+
 @awkward.mixin_class(behavior)
 class MomentumCandidate(vector.LorentzVector):
     """A Lorentz vector with charge
@@ -127,43 +150,21 @@ MomentumCandidateArray.ProjectionClass3D = vector.ThreeVectorArray  # noqa: F821
 MomentumCandidateArray.ProjectionClass4D = vector.LorentzVectorArray  # noqa: F821
 MomentumCandidateArray.MomentumClass = MomentumCandidateArray  # noqa: F821
 
-
 @awkward.mixin_class(behavior)
 class MCTruthParticle(MomentumCandidate, base.NanoCollection):
     """Generated Monte Carlo particles."""
 
-
-    @numba.njit
-    def index_range_numba_wrap(self, begin_end, builder):
-        for ev in begin_end:
-            builder.begin_list()
-            for j in ev:
-                builder.begin_list()
-                for k in range(j[0],j[1]):
-                    builder.integer(k)
-                builder.end_list()
-            builder.end_list()
-        return builder
-
-    def index_range(self, begin, end):
-        begin_end = awkward.concatenate((begin[:,:,numpy.newaxis],end[:,:,numpy.newaxis]),axis=2)
-        if awkward.backend(begin) == "typetracer" or awkward.backend(end) == "typetracer":
-            # here we fake the output of numba wrapper function since
-            # operating on length-zero data returns the wrong layout!
-            awkward.typetracer.length_zero_if_typetracer(begin_end) # force touching of the necessary data
-            return awkward.Array(awkward.Array([]).layout.to_typetracer(forget_length=True))
-        return self.index_range_numba_wrap(begin_end, awkward.ArrayBuilder()).snapshot()
-
     #Daughters
     @dask_property
     def get_daughters_index(self):
-        ranges = self.index_range(self.daughters.begin, self.daughters.end)
+        ranges = index_range(self.daughters.begin, self.daughters.end)
         return awkward.values_astype(map_index_to_array(self._events().Particleidx1.index, ranges, axis=2), "int64")
 
     @get_daughters_index.dask
     def get_daughters_index(self, dask_array):
-        ranges = dask_awkward.map_partitions(self.index_range, dask_array.daughters.begin, dask_array.daughters.end)
-        return awkward.values_astype(map_index_to_array(dask_array._events().Particleidx1.index, ranges, axis=2), "int64")
+        ranges = dask_awkward.map_partitions(index_range, dask_array.daughters.begin, dask_array.daughters.end)
+        daughters = dask_awkward.map_partitions(map_index_to_array, dask_array._events().Particleidx1.index, ranges, axis=2)
+        return awkward.values_astype(daughters, "int32")
 
     @dask_property
     def get_daughters(self):
@@ -176,13 +177,14 @@ class MCTruthParticle(MomentumCandidate, base.NanoCollection):
     #Parents
     @dask_property
     def get_parents_index(self):
-        ranges = self.index_range(self.parents.begin, self.parents.end)
+        ranges = index_range(self.parents.begin, self.parents.end)
         return awkward.values_astype(map_index_to_array(self._events().Particleidx0.index, ranges, axis=2), "int64")
 
     @get_parents_index.dask
     def get_parents_index(self, dask_array):
-        ranges = dask_awkward.map_partitions(self.index_range, dask_array.parents.begin, dask_array.parents.end)
-        return awkward.values_astype(map_index_to_array(dask_array._events().Particleidx0.index, ranges, axis=2), "int64")
+        ranges = dask_awkward.map_partitions(index_range, dask_array.parents.begin, dask_array.parents.end)
+        daughters = dask_awkward.map_partitions(map_index_to_array, dask_array._events().Particleidx0.index, ranges, axis=2)
+        return awkward.values_astype(daughters, "int32")
 
     @dask_property
     def get_parents(self):
@@ -191,121 +193,6 @@ class MCTruthParticle(MomentumCandidate, base.NanoCollection):
     @get_parents.dask
     def get_parents(self, dask_array):
         return map_index_to_array(dask_array, dask_array.get_parents_index, axis=2)
-
-    # @property
-    # def matched_pfos(self, _dask_array_=None):
-    #     """Returns an array of matched reconstructed particle objects for each generator particle."""
-    #     if _dask_array_ is not None:
-    #         collection_name = self.layout.purelist_parameter("collection_name")
-    #         original_from = self.behavior["__original_array__"]()[collection_name]
-    #         original = self.behavior["__original_array__"]().PandoraPFOs
-    #         return original._apply_global_mapping(
-    #             _dask_array_,
-    #             original_from,
-    #             self.behavior["__original_array__"]().RecoMCTruthLink.Gmc_index,
-    #             self.behavior["__original_array__"]().RecoMCTruthLink.Greco_index,
-    #             _dask_array_=original,
-    #         )
-    #     raise RuntimeError("Not reachable in dask mode!")
-
-    # @property
-    # def matched_clusters(self, _dask_array_=None):
-    #     """Returns an array of matched cluster particle objects for each generator particle."""
-    #     if _dask_array_ is not None:
-    #         collection_name = self.layout.purelist_parameter("collection_name")
-    #         original_from = self.behavior["__original_array__"]()[collection_name]
-    #         original = self.behavior["__original_array__"]().PandoraClusters
-    #         return original._apply_global_mapping(
-    #             _dask_array_,
-    #             original_from,
-    #             self.behavior["__original_array__"]().ClusterMCTruthLink.Gmc_index,
-    #             self.behavior["__original_array__"]().ClusterMCTruthLink.Gcluster_index,
-    #             _dask_array_=original,
-    #         )
-    #     raise RuntimeError("Not reachable in dask mode!")
-
-    # @property
-    # def matched_trks(self, _dask_array_=None):
-    #     """Returns an array of matched cluster particle objects for each generator particle."""
-    #     if _dask_array_ is not None:
-    #         collection_name = self.layout.purelist_parameter("collection_name")
-    #         original_from = self.behavior["__original_array__"]()[collection_name]
-    #         original = self.behavior["__original_array__"]().MarlinTrkTracks
-    #         return original._apply_global_mapping(
-    #             _dask_array_,
-    #             original_from,
-    #             self.behavior[
-    #                 "__original_array__"
-    #             ]().MarlinTrkTracksMCTruthLink.Gmc_index,
-    #             self.behavior[
-    #                 "__original_array__"
-    #             ]().MarlinTrkTracksMCTruthLink.Gtrk_index,
-    #             _dask_array_=original,
-    #         )
-    #     raise RuntimeError("Not reachable in dask mode!")
-
-    # def _apply_nested_global_index(self, index, nested_counts, _dask_array_=None):
-    #     """As _apply_global_index but expects one additional layer of nesting to get specified."""
-    #     if isinstance(index, int):
-    #         out = self._content()[index]
-    #         return awkward.Record(out, behavior=self.behavior)
-
-    #     def flat_take(layout):
-    #         idx = awkward.Array(layout)
-    #         return self._content()[idx.mask[idx >= 0]]
-
-    #     def descend(layout, depth, **kwargs):
-    #         if layout.purelist_depth == 1:
-    #             return flat_take(layout)
-
-    #     (index_out,) = awkward.broadcast_arrays(
-    #         index._meta if isinstance(index, dask_awkward.Array) else index
-    #     )
-    #     nested_counts_out = (
-    #         nested_counts._meta
-    #         if isinstance(nested_counts, dask_awkward.Array)
-    #         else nested_counts
-    #     )
-    #     index_out = awkward.unflatten(
-    #         index_out, awkward.flatten(nested_counts_out), axis=-1
-    #     )
-    #     layout_out = awkward.transform(descend, index_out.layout, highlevel=False)
-    #     out = awkward.Array(layout_out, behavior=self.behavior)
-
-    #     if isinstance(index, dask_awkward.Array):
-    #         return _dask_array_.map_partitions(
-    #             base._ClassMethodFn("_apply_nested_global_index"),
-    #             index,
-    #             nested_counts,
-    #             label="_apply_nested_global_index",
-    #             meta=out,
-    #         )
-    #     return out
-
-    # @property
-    # def parents(self, _dask_array_=None):
-    #     if _dask_array_ is not None:
-    #         collection_name = self.layout.purelist_parameter("collection_name")
-    #         original = self.behavior["__original_array__"]()[collection_name]
-    #         return original._apply_nested_global_index(
-    #             self.behavior["__original_array__"]().GMCParticlesSkimmedParentsIndex,
-    #             original.parents_counts,
-    #             _dask_array_=original,
-    #         )
-    #     raise RuntimeError("Not reachable in dask mode!")
-
-    # @property
-    # def children(self, _dask_array_=None):
-    #     if _dask_array_ is not None:
-    #         collection_name = self.layout.purelist_parameter("collection_name")
-    #         original = self.behavior["__original_array__"]()[collection_name]
-    #         return original._apply_nested_global_index(
-    #             self.behavior["__original_array__"]().GMCParticlesSkimmedParentsIndex,
-    #             original.children_counts,
-    #             _dask_array_=original,
-    #         )
-    #     raise RuntimeError("Not reachable in dask mode!")
-
 
 _set_repr_name("MCTruthParticle")
 behavior.update(
